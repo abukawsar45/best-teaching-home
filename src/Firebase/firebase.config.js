9ef32e0e98603aaae520747c068bf2c6b2399363
abukawsar45 <kawsar1146236@gmail.com>
2023-06-06 23:50:16 +0600

    react router and tailwind set up

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAAcJLNyyEF8eb5Ih_7e_kcI7rMk-ncqx0',
+  authDomain: 'best-teaching-home.firebaseapp.com',
+  projectId: 'best-teaching-home',
+  storageBucket: 'best-teaching-home.appspot.com',
+  messagingSenderId: '609057399612',
+  appId: '1:609057399612:web:f17539bdaa5dbe07fbf439',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/Routes/router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <div className=" bg-orange-400">hey</div>
+  }
+])
+export default router
