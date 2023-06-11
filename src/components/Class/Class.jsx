825b4258b023a43c3f27fe6642bc56d3b685efc9
abukawsar45 <kawsar1146236@gmail.com>
2023-06-11 08:47:53 +0600

    show approved cart done

--- src/components/Class/Class.jsx
+++ src/components/Class/Class.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import useClass from "../../hooks/useClass"
 import axios from "axios";
+import ClassCart from "../ClassCart/ClassCart";
 
 const Class = () => {
   const [allClass, setAllClass] = useState([]);
@@ -18,7 +19,17 @@ const Class = () => {
     
   } ,[])
   return (
-    <div>Class</div>
+    <div>
+      <div>
+        <p className="text-2xl text-center my-4 text-sky-400">All Classes Here: {allClass?.length} </p>
+      </div>
+      <div>
+
+        {
+          allClass.map((cart => <ClassCart key={ cart._id } cart={ cart }/> ))
+        }
+      </div>
+    </div>
   )
 }
 
--- src/components/ClassCart/ClassCart.jsx
+++ src/components/ClassCart/ClassCart.jsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
 
-const ClassCart = () => {
+const ClassCart = ({cart}) => {
   return (
-    <div>
-      cart
+    <div className='my-2 md:my-4 lg:my-8'>
+      <div className={`hero   ${(cart?.availableSeat > 0)?'bg-base-200':'bg-red-700'}`}>
+        <div className='hero-content grid grid-cols-1 md:grid-cols-2 '>
+          <div>
+             <img
+            src={cart.classImage}
+            className='max-w-sm rounded-lg shadow-2xl'
+          />
+         </div>
+          <div>
+            <h1 className='text-5xl font-bold'>{cart?.className} </h1>
+            <h3 className='text-2xl font-medium mt-2'>
+              {cart?.instructorName}{' '}
+            </h3>
+            <h3 className='text-2xl font-medium mt-2'>
+              Available Seat: {cart?.availableSeat}{' '}
+            </h3>
+            <h3 className='text-2xl font-medium mt-2'>
+              Price: $ {cart?.price}{' '}
+            </h3>
+
+            <Link className='btn btn-primary'>Select</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
